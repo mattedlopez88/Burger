232220420a9f9fccb686f38367cd84c5d2757ebc
laboratorio <laboratorio@UPEAULA-31712>
2024-10-21 10:14:23 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Burger
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }//? lo convierte en un campo obligaotio
+        public bool withCheese { get; set; }
+        public decimal Price { get; set; }
+    }
+}
